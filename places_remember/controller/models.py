b6067c8cd8d6228e443bcb7a25fc9bf673b9a768
Dmitry Grishin <grishinda10@ya.ru>
2021-12-22 18:57:11 +0500

    made user memories file

--- places_remember/controller/models.py
+++ places_remember/controller/models.py
@@ -1,2 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import CASCADE
 
 
+def user_directory_path(instance, filename):
+    return f'users/user_{instance.user.id}/{instance.user.id}_{filename}'
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    memories = models.FileField(upload_to=user_directory_path,
+                                verbose_name='Воспоминания',
+                                auto_created=True)
+
+    def __unicode__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
